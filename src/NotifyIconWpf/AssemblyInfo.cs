0a9458243db9a70331e595d4f1ec36f3cd7b3370
Aarthi Subhakar <aarthi.subhakar@gmail.com>
2020-05-21 13:59:30 +0530

    Update to .Net3.1

--- src/NotifyIconWpf/AssemblyInfo.cs
+++ src/NotifyIconWpf/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
@@ -24,11 +24,11 @@ using System.Windows.Markup;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page, 
-    // or application resource dictionaries)
+                                     //(used if a resource is not found in the page, 
+                                     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page, 
-    // app, or any theme specific resource dictionaries)
+                                              //(used if a resource is not found in the page, 
+                                              // app, or any theme specific resource dictionaries)
     )]
 
 
@@ -42,4 +42,3 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
